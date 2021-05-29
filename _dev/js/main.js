070d2f253b03aafebcfd58e0bb01d5db6aa5c7f8
Alexis Bertin <alexisbrtn@hotmail.com>
2021-05-29 20:53:22 +0200

    home finished

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -6,7 +6,18 @@ let $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
+// IMPORT OTHER PACKAGES
+import lazyframe from "lazyframe";
+
 //LET'S SCRIPT, OH YEAH !
 jQuery( document ).ready(function() {
+
+    //LAZYLOAD IFRAME
+    let elements = jQuery(".lazyframe");
+    lazyframe(elements, {
+        debounce: 250,
+        lazyload: true,
+        autoplay: true
+    });
 
 })
--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -7,9 +7,12 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('../public/build/')
+
     // public path used by the web server to access the output path
-    .setPublicPath('/build/')
-    .setManifestKeyPrefix('./')
+    .setPublicPath('.')
+
+    // only needed for CDN's or sub-directory deploy
+    .setManifestKeyPrefix('public/build/')
 
     .addEntry('app', './js/main.js')
 
@@ -30,6 +33,12 @@ Encore
         from: './fonts',
         to: 'fonts/[path][name].[ext]',
         pattern: /\.(eot|svg|otf|ttf|woff)$/
+    })
+
+    .copyFiles({
+        from: './img',
+        to: 'images/[path][name].[hash:8].[ext]',
+        pattern: /\.(png|jpg|jpeg|svg)$/
     })
 ;
 
