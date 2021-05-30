765dfe3f16f312c7af9708bb754bfb48f8e3cfef
Alexis Bertin <alexisbrtn@hotmail.com>
2021-05-30 20:12:38 +0200

    add audio feature

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -8,6 +8,7 @@ window.jQuery = $;
 
 // IMPORT OTHER PACKAGES
 import lazyframe from "lazyframe";
+import { CircleAudioPlayer } from '../js/audio-player';
 
 //LET'S SCRIPT, OH YEAH !
 jQuery( document ).ready(function() {
@@ -19,5 +20,19 @@ jQuery( document ).ready(function() {
         lazyload: true,
         autoplay: true
     });
+
+    //AUDIO PLAYERS
+    jQuery('.player-container').on('click', function(){
+        jQuery(this).toggleClass('active');
+    })
+    //AUDIO #1
+    let src1 = jQuery('#sound1').attr('src');
+    let player1 = new CircleAudioPlayer({
+        audio: src1,
+        size: 120,
+        borderWidth: 8
+    });
+    player1.appendTo(playerContainer1);
+
 
 })
